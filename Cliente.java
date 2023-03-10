b3c5a491ea40619b811c9ed800abea6eac13d257
Kaua Rodrigues dos Santos <kaua.santos@germinare.org.br>
2023-03-10 19:25:54 -0300

    Corrigindo conflitos do merge

--- Cliente.java
+++ Cliente.java
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 public class Cliente {
     private String tipoConta;
-=======
- public class Cliente {
     private String nome;
     private String cpf;
     private int agencia;
     private double saldo;
-    
->>>>>>> classe-cliente
+
 }
